26c9aeede65c4907987be7e0c6cb13af972f84d1
owlboy-lin <298076473@qq.com>
2025-03-14 12:58:43 +0800

    update

--- smpackage/Lean-all-iStore-2-test.sh
+++ smpackage/Lean-all-iStore-2-test.sh
@@ -136,16 +136,15 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-rm -rf feeds/packages/net/{adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns,lucky}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/luci/applications/{luci-app-alist,luci-app-lucky}
-git clone --depth 1 https://github.com/kenzok8/small-package.git package/kz8-small
-mv package/kz8-small/adguardhome package/adguardhome
+rm -rf feeds/packages/net/adguardhome
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
-mv package/kz8-small/lucky package/lucky
-mv package/kz8-small/luci-app-lucky package/luci-app-lucky
-mv package/kz8-small/smartdns package/smartdns
+mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
+mv package/kz8-small/luci-app-partexp package/luci-app-partexp
+mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+mv package/kz8-small/wrtbwmon package/wrtbwmon
 mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
+mv package/kz8-small/homebox package/homebox
 mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
 rm -rf package/kz8-small
 
