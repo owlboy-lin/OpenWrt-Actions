e18047cbb3c83a2a01a2d32f8b3f5398531dfa1f
owlboy-lin <298076473@qq.com>
2025-03-15 21:55:45 +0800

    update

--- bpi-r4/Banana-Pi-BPI-R4-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-2.sh
@@ -253,21 +253,24 @@ EOF
 echo "
 
 # 额外组件
-CONFIG_GRUB_IMAGES=y
-CONFIG_VMDK_IMAGES=y
+# CONFIG_GRUB_IMAGES=y
+# CONFIG_VMDK_IMAGES=y
 
-CONFIG_TARGET_ROOTFS_EXT4FS=y
-CONFIG_TARGET_EXT4_RESERVED_PCT=0
-CONFIG_TARGET_EXT4_BLOCKSIZE_4K=y
-# CONFIG_TARGET_EXT4_BLOCKSIZE_2K is not set
-# CONFIG_TARGET_EXT4_BLOCKSIZE_1K is not set
-CONFIG_TARGET_EXT4_BLOCKSIZE=4096
-# CONFIG_TARGET_EXT4_JOURNAL is not set
+# CONFIG_TARGET_ROOTFS_EXT4FS=y
+# CONFIG_TARGET_EXT4_RESERVED_PCT=0
+# CONFIG_TARGET_EXT4_BLOCKSIZE_4K=y
+# # CONFIG_TARGET_EXT4_BLOCKSIZE_2K is not set
+# # CONFIG_TARGET_EXT4_BLOCKSIZE_1K is not set
+# CONFIG_TARGET_EXT4_BLOCKSIZE=4096
+# # CONFIG_TARGET_EXT4_JOURNAL is not set
 
 
 # 固件大小
-CONFIG_TARGET_KERNEL_PARTSIZE=1024
-CONFIG_TARGET_ROOTFS_PARTSIZE=1024
+# CONFIG_TARGET_KERNEL_PARTSIZE=1024
+# CONFIG_TARGET_ROOTFS_PARTSIZE=1024
+
+CONFIG_TARGET_SQUASHFS_BLOCK_SIZE=512
+
 
 
 # # Themes
@@ -297,6 +300,8 @@ CONFIG_PACKAGE_luci-app-mosdns=y
 # netspeedtest chmod +x /etc/init.d/netspeedtest
 CONFIG_PACKAGE_luci-app-netspeedtest=y
 
+# 宽带监控
+CONFIG_PACKAGE_luci-app-nlbwmon=y
 
 # passwall
 CONFIG_PACKAGE_luci-app-passwall=y
@@ -305,11 +310,9 @@ CONFIG_PACKAGE_luci-app-passwall=y
 CONFIG_PACKAGE_luci-app-passwall2=y
 
 
-# luci-app-ssr-plus
-CONFIG_PACKAGE_luci-app-ssr-plus=y
 
 # quickstart
-CONFIG_PACKAGE_luci-app-quickstart=n
+CONFIG_PACKAGE_luci-app-quickstart=y
 
 
 # store
@@ -326,8 +329,9 @@ luci-app-uugamebooster=n
 CONFIG_PACKAGE_luci-app-webadmin=y
 
 
-
+CONFIG_DEFAULT_autosamba=n
 CONFIG_PACKAGE_autosamba=n
+CONFIG_PACKAGE_autosamba_INCLUDE_KSMBD=n
 CONFIG_PACKAGE_luci-app-accesscontrol=n
 CONFIG_PACKAGE_luci-app-arpbind=n
 CONFIG_PACKAGE_luci-app-ddns=n
