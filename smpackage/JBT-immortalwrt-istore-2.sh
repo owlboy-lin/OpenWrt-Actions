15b3a7636801a6614d635819b74ae001f7f27e97
owlboy-lin <298076473@qq.com>
2025-03-15 13:55:14 +0800

    Update JBT-immortalwrt-istore-2.sh

--- smpackage/JBT-immortalwrt-istore-2.sh
+++ smpackage/JBT-immortalwrt-istore-2.sh
@@ -94,19 +94,18 @@
 # svn export https://github.com/kiddin9/openwrt-packages/trunk/mosdns package/mosdns
 # svn export https://github.com/kiddin9/openwrt-packages/trunk/v2dat package/v2dat
 
-# sed -i 's/192.168.1.1/192.168.24.1/g' package/base-files/files/bin/config_generate
-# sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.24.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-# sed -i 's/192.168.1.1/192.168.24.1/g' package/base-files/luci2/bin/config_generate
-# sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
-# sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
-# sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# #sed -i "s/luci-theme-bootstrap/luci-theme-design/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+# sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+# sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+# sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+# sed -i 's#mirrors.vsean.net/openwrt#mirrors.pku.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
+# mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
+# mv $GITHUB_WORKSPACE/patch/immortalwrt-24.10/199-diy.sh package/base-files/files/etc/uci-defaults/199-diy.sh
 
-# sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
-# sed -i '/tencent.com/d' package/lean/default-settings/files/zzz-default-settings
-# sed -i '/shadow/d' package/lean/default-settings/files/zzz-default-settings
-# mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
-# mv $GITHUB_WORKSPACE/patch/lean/199-diy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
+#完全删除luci版本
+# sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+#添加编译日期
+# sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/usr/lib/os-release
+# sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/etc/openwrt_release
 
 # git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
 # git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
@@ -130,9 +129,10 @@ git clone --depth 1 -b main https://github.com/linkease/istore.git package/istor
 #sed -i 's/\"network\"/\"modem\"/g' package/5g-modem/luci-app-modem/luasrc/controller/modem.lua
 
 # # #mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
-
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
@@ -142,22 +142,32 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone -b master --depth 1 https://github.com/fw876/helloworld.git package/helloworld
 
 
-rm -rf feeds/packages/net/adguardhome
+#rm -rf feeds/packages/net/adguardhome
 # #adguardhome
 # git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git package/luci-app-adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth 1 https://github.com/kenzok78/luci-app-adguardhome package/luci-app-adguardhome
 
+rm -rf feeds/packages/net/adguardhome
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
-# mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
-# mv package/kz8-small/luci-app-partexp package/luci-app-partexp
-# mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
-# mv package/kz8-small/wrtbwmon package/wrtbwmon
+mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
+mv package/kz8-small/luci-app-partexp package/luci-app-partexp
+mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+mv package/kz8-small/wrtbwmon package/wrtbwmon
 mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
 mv package/kz8-small/homebox package/homebox
 mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
 rm -rf package/kz8-small
+
+
+# git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci package/imm23luci
+# mv package/imm23luci/applications/luci-app-adbyby-plus package/luci-app-adbyby-plus
+# rm -rf package/imm23luci
+# git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/packages package/imm23packages
+# mv package/imm23packages/net/adbyby package/adbyby
+# rm -rf package/imm23packages
 
 # git clone --depth 1 -b lua https://github.com/sbwml/luci-app-alist package/alist
 # rm -rf package/alist/alist
