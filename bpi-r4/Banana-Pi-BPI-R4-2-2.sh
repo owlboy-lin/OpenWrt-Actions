f1b394bc979969c5c090a573cedbf8030f2f92f4
owlboy-lin <298076473@qq.com>
2025-03-16 18:42:15 +0800

    update

--- bpi-r4/Banana-Pi-BPI-R4-2-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-2-2.sh
@@ -1,9 +1,9 @@
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.89.249/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.89.249/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/Router/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/WiFi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
-mv $GITHUB_WORKSPACE/patch/immortalwrt-23.05/199-diy.sh package/base-files/files/etc/uci-defaults/199-diy.sh
+# mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
+# mv $GITHUB_WORKSPACE/patch/immortalwrt-23.05/199-diy.sh package/base-files/files/etc/uci-defaults/199-diy.sh
 
 #完全删除luci版本
 sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
@@ -17,9 +17,9 @@ sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/etc/
 # git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 #rm -rf feeds/packages/net/quectel-cm
 
-rm -rf feeds/packages/lang/golang
-rm -rf feeds/packages/devel/gn
-rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,geoview,hysteria,ipt2socks,microsocks,naiveproxy,pdnsd-alt,shadowsocks-rust,shadowsocksr-libev,simple-obfs,sing-box,ssocks,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns}
+# rm -rf feeds/packages/lang/golang
+# rm -rf feeds/packages/devel/gn
+# rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,geoview,hysteria,ipt2socks,microsocks,naiveproxy,pdnsd-alt,shadowsocks-rust,shadowsocksr-libev,simple-obfs,sing-box,ssocks,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns}
 # rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
 git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 # git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
