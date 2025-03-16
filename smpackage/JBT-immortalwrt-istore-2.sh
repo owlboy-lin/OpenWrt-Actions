908d8f59ff6a3bd1c2339af2221b0762fb402b3b
owlboy-lin <298076473@qq.com>
2025-03-16 15:19:06 +0800

    Update JBT-immortalwrt-istore-2.sh

--- smpackage/JBT-immortalwrt-istore-2.sh
+++ smpackage/JBT-immortalwrt-istore-2.sh
@@ -117,7 +117,7 @@ sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/etc/
 # git clone --depth 1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+# git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 # git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
 # git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
 # mv package/nas-packages/network/services/* package/nas-packages/
