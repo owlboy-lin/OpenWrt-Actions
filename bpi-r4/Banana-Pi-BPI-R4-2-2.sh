17d8934dde0681d21fe44488c8c7786caa382742
owlboy-lin <298076473@qq.com>
2025-03-16 17:55:54 +0800

    Update Banana-Pi-BPI-R4-2-2.sh

--- bpi-r4/Banana-Pi-BPI-R4-2-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-2-2.sh
@@ -20,15 +20,15 @@ git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/devel/gn
 rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,geoview,hysteria,ipt2socks,microsocks,naiveproxy,pdnsd-alt,shadowsocks-rust,shadowsocksr-libev,simple-obfs,sing-box,ssocks,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns}
-rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
+# rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
 git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
 git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+# git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
 
 #rm -rf feeds/packages/lang/rust
 #git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/packages package/immpkg
