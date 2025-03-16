ecba14be603228b77c7a579d9c410ecc0b711f12
owlboy-lin <298076473@qq.com>
2025-03-16 15:13:13 +0800

    Update Banana-Pi-BPI-R4-2-1.sh

--- bpi-r4/Banana-Pi-BPI-R4-2-1.sh
+++ bpi-r4/Banana-Pi-BPI-R4-2-1.sh
@@ -14,7 +14,7 @@ sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/etc/
 #git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 #rm -rf feeds/packages/net/quectel-cm
 
-# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 # git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 # git clone --depth 1 -b main https://github.com/linkease/istore.git package/istore
