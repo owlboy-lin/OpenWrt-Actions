22d79fa0bda9be401c7027a16fa3773b4891e333
owlboy-lin <298076473@qq.com>
2025-03-17 10:59:03 +0800

    Update Banana-Pi-BPI-R4-2.sh

--- bpi-r4/Banana-Pi-BPI-R4-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-2.sh
@@ -14,8 +14,8 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/l
 #git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 #rm -rf feeds/packages/net/quectel-cm
 
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 # git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
