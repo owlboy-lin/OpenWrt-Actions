69aa486c2b5a791e7e8fe177ee0451b793cca3ca
owlboy-lin <298076473@qq.com>
2025-03-17 22:33:24 +0800

    Update bllw-Lean-2.sh

--- smpackage/bllw-Lean-2.sh
+++ smpackage/bllw-Lean-2.sh
@@ -192,42 +192,42 @@ sed -i "s#192.168.1.1#192.168.89.249#g" $NET                                    
 
 # ================ 网络设置 =======================================
 
-cat >> $ZZZ <<-EOF
-# 设置网络-旁路由模式
-uci set network.lan.gateway='192.168.89.248'                     # 旁路由设置 IPv4 网关
-uci set network.lan.dns='223.5.5.5 119.29.29.29'            # 旁路由设置 DNS(多个DNS要用空格分开)
-uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
-uci delete network.lan.type                                  # 旁路由桥接模式-禁用
-uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
-uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
+# cat >> $ZZZ <<-EOF
+# # 设置网络-旁路由模式
+# uci set network.lan.gateway='192.168.89.248'                     # 旁路由设置 IPv4 网关
+# uci set network.lan.dns='223.5.5.5 119.29.29.29'            # 旁路由设置 DNS(多个DNS要用空格分开)
+# uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
+# uci delete network.lan.type                                  # 旁路由桥接模式-禁用
+# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
+# uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
 
-# 设置防火墙-旁路由模式
-uci set firewall.@defaults[0].syn_flood='0'                  # 禁用 SYN-flood 防御
-uci set firewall.@defaults[0].flow_offloading='0'           # 禁用基于软件的NAT分载
-uci set firewall.@defaults[0].flow_offloading_hw='0'       # 禁用基于硬件的NAT分载
-uci set firewall.@defaults[0].fullcone='0'                   # 禁用 FullCone NAT
-uci set firewall.@defaults[0].fullcone6='0'                  # 禁用 FullCone NAT6
-uci set firewall.@zone[0].masq='1'                             # 启用LAN口 IP 动态伪装
+# # 设置防火墙-旁路由模式
+# uci set firewall.@defaults[0].syn_flood='0'                  # 禁用 SYN-flood 防御
+# uci set firewall.@defaults[0].flow_offloading='0'           # 禁用基于软件的NAT分载
+# uci set firewall.@defaults[0].flow_offloading_hw='0'       # 禁用基于硬件的NAT分载
+# uci set firewall.@defaults[0].fullcone='0'                   # 禁用 FullCone NAT
+# uci set firewall.@defaults[0].fullcone6='0'                  # 禁用 FullCone NAT6
+# uci set firewall.@zone[0].masq='1'                             # 启用LAN口 IP 动态伪装
 
-# 旁路IPV6需要全部禁用
-uci del network.lan.ip6assign                                 # IPV6分配长度-禁用
-uci del dhcp.lan.ra                                             # 路由通告服务-禁用
-uci del dhcp.lan.dhcpv6                                        # DHCPv6 服务-禁用
-uci del dhcp.lan.ra_management                               # DHCPv6 模式-禁用
+# # 旁路IPV6需要全部禁用
+# uci del network.lan.ip6assign                                 # IPV6分配长度-禁用
+# uci del dhcp.lan.ra                                             # 路由通告服务-禁用
+# uci del dhcp.lan.dhcpv6                                        # DHCPv6 服务-禁用
+# uci del dhcp.lan.ra_management                               # DHCPv6 模式-禁用
 
-# 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
-uci set network.ipv6=interface
-uci set network.ipv6.proto='dhcpv6'
-uci set network.ipv6.ifname='@lan'
-uci set network.ipv6.reqaddress='try'
-uci set network.ipv6.reqprefix='auto'
-uci set firewall.@zone[0].network='lan ipv6'
+# # 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
+# uci set network.ipv6=interface
+# uci set network.ipv6.proto='dhcpv6'
+# uci set network.ipv6.ifname='@lan'
+# uci set network.ipv6.reqaddress='try'
+# uci set network.ipv6.reqprefix='auto'
+# uci set firewall.@zone[0].network='lan ipv6'
 
-uci commit dhcp
-uci commit network
-uci commit firewall
+# uci commit dhcp
+# uci commit network
+# uci commit firewall
 
-EOF
+# EOF
 
 
 
