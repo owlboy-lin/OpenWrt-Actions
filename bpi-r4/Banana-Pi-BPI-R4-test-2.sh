60a20a3e051bdb1c0ce84aaa4d488cecd65c77f5
owlboy-lin <298076473@qq.com>
2025-03-18 14:53:16 +0800

    Update Banana-Pi-BPI-R4-test-2.sh

--- bpi-r4/Banana-Pi-BPI-R4-test-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-test-2.sh
@@ -15,7 +15,7 @@ CONFIG_PACKAGE_luci-app-autoreboot=y
 
 
 # 关机
-CONFIG_PACKAGE_luci-app-poweroff=y
+# CONFIG_PACKAGE_luci-app-poweroff=y
 
 
 # openclash
@@ -23,15 +23,15 @@ CONFIG_PACKAGE_luci-app-openclash=y
 
 
 # adguardhome
-CONFIG_PACKAGE_luci-app-adguardhome=y
-CONFIG_PACKAGE_luci-app-adguardhome_INCLUDE_binary=y
+# CONFIG_PACKAGE_luci-app-adguardhome=y
+# CONFIG_PACKAGE_luci-app-adguardhome_INCLUDE_binary=y
 
 # mosdns
-CONFIG_PACKAGE_luci-app-mosdns=y
+# CONFIG_PACKAGE_luci-app-mosdns=y
 
 
 # netspeedtest chmod +x /etc/init.d/netspeedtest
-CONFIG_PACKAGE_luci-app-netspeedtest=y
+# CONFIG_PACKAGE_luci-app-netspeedtest=y
 
 # 宽带监控
 CONFIG_PACKAGE_luci-app-nlbwmon=y
@@ -40,24 +40,22 @@ CONFIG_PACKAGE_luci-app-nlbwmon=y
 CONFIG_PACKAGE_luci-app-passwall=y
 
 
-CONFIG_PACKAGE_luci-app-passwall2=n
-
 # luci-app-ssr-plus
 CONFIG_PACKAGE_luci-app-ssr-plus=y
 
 
 # quickstart
-CONFIG_PACKAGE_luci-app-quickstart=n
+# CONFIG_PACKAGE_luci-app-quickstart=n
 
 
 # store
-CONFIG_PACKAGE_luci-app-store=y
+# CONFIG_PACKAGE_luci-app-store=y
 
 # luci-app-ttyd=y
 CONFIG_PACKAGE_luci-app-ttyd=y
 
 # luci-app-uugamebooster
-CONFIG_PACKAGE_luci-app-uugamebooster=n
+# CONFIG_PACKAGE_luci-app-uugamebooster=n
 
 # luci-app-vssr
 CONFIG_PACKAGE_luci-app-vssr=y
@@ -65,19 +63,6 @@ CONFIG_PACKAGE_luci-app-vssr=y
 # luci-app-webadmin=y
 CONFIG_PACKAGE_luci-app-webadmin=y
 
-
-# CONFIG_DEFAULT_autosamba is not set
-# CONFIG_PACKAGE_autosamba is not set
-# CONFIG_PACKAGE_autosamba_INCLUDE_KSMBD is not set
-# CONFIG_PACKAGE_luci-app-accesscontrol is not set
-# CONFIG_PACKAGE_luci-app-ksmbd is not set
-# CONFIG_PACKAGE_luci-app-arpbind is not set
-# CONFIG_PACKAGE_luci-app-ddns is not set
-# CONFIG_PACKAGE_luci-app-samba4 is not set
-# CONFIG_PACKAGE_luci-app-upnp is not set
-# CONFIG_PACKAGE_luci-app-vsftpd is not set
-# CONFIG_PACKAGE_luci-app-wol is not set
-CONFIG_PACKAGE_swconfig=y
 
 
 
