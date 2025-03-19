09cbdd1764d523bf373ab62036a33a6e7d13a52d
GitHub Action <github-actions[bot]@github.com>
2025-03-19 15:59:33 +0000

    Sync files 4b39d059dbc69eefc1bf012ebecae70fe065b18b

--- bpi-r4/Banana-Pi-BPI-R4-test-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-test-2.sh
@@ -13,41 +13,41 @@
 rm -rf package/istore/luci-app-store
 
 # iStore
-# git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 # git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
 # git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
 # mv package/nas-packages/network/services/* package/nas-packages/
 # rm -rf package/nas-packages/network
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+# find ./ | grep Makefile | grep mosdns | xargs rm -f
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+# git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
 # git clone --depth 1 https://github.com/vernesong/OpenClash.git package/OpenClash
 # git clone --depth 1 https://github.com/morytyann/OpenWrt-mihomo.git package/mihomo
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 
 # rm -rf feeds/packages/devel/gn
 # rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
 
-rm -rf feeds/packages/net/adguardhome
-git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
-mv package/kz8-small/adguardhome package/adguardhome
-mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
-mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
-mv package/kz8-small/luci-app-partexp package/luci-app-partexp
-mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
-mv package/kz8-small/wrtbwmon package/wrtbwmon
-mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
-mv package/kz8-small/homebox package/homebox
-mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
-rm -rf package/kz8-small
+# rm -rf feeds/packages/net/adguardhome
+# git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+# mv package/kz8-small/adguardhome package/adguardhome
+# mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+# mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
+# mv package/kz8-small/luci-app-partexp package/luci-app-partexp
+# mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+# mv package/kz8-small/wrtbwmon package/wrtbwmon
+# mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
+# mv package/kz8-small/homebox package/homebox
+# mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
+# rm -rf package/kz8-small
 
 # git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci package/imm23luci
 # mv package/imm23luci/applications/luci-app-adbyby-plus package/luci-app-adbyby-plus
@@ -87,7 +87,6 @@ sed -i "s#192.168.1.1#192.168.24.9#g" $NET3                                     
 echo "
 
 # 额外组件
-
 CONFIG_TARGET_SQUASHFS_BLOCK_SIZE=512
 
 
