5d6810ade5ed9a13d3ab069ee44baa0df9024982
owlboy-lin <298076473@qq.com>
2025-03-20 19:34:33 +0800

    Update Banana-Pi-BPI-R4-test-2.sh

--- bpi-r4/Banana-Pi-BPI-R4-test-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-test-2.sh
@@ -33,7 +33,7 @@ git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
 # git clone --depth 1 https://github.com/vernesong/OpenClash.git package/OpenClash
 # git clone --depth 1 https://github.com/morytyann/OpenWrt-mihomo.git package/mihomo
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # t clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 
 # rm -rf feeds/packages/devel/gn
