fdff4f0c4c324e5e44c2d256a37e657a59793b7a
owlboy-lin <298076473@qq.com>
2025-03-21 19:36:16 +0800

    Update Banana-Pi-BPI-R4-test-2.sh

--- bpi-r4/Banana-Pi-BPI-R4-test-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-test-2.sh
@@ -40,10 +40,10 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 # rm -rf feeds/packages/devel/gn
 # rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
 
-# rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/adguardhome
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-# git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 
 
@@ -52,7 +52,7 @@ git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git p
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+# git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
 # mv package/kz8-small/adguardhome package/adguardhome
 # mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
 # mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
@@ -61,7 +61,7 @@ git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-sma
 # mv package/kz8-small/wrtbwmon package/wrtbwmon
 # mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
 # mv package/kz8-small/homebox package/homebox
-mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
+# mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
 # rm -rf package/kz8-small
 
 # git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci package/imm23luci
