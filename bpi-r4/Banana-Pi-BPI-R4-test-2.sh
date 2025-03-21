38fd1b7e12947a10e2ee8cde7df87eb7aeba505f
owlboy-lin <298076473@qq.com>
2025-03-21 22:54:23 +0800

    Update Banana-Pi-BPI-R4-test-2.sh

--- bpi-r4/Banana-Pi-BPI-R4-test-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-test-2.sh
@@ -41,8 +41,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 # rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
 
 rm -rf feeds/packages/net/adguardhome
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 
