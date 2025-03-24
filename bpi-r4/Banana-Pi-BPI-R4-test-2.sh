a913f6682f13cca31588b7a576ac088e53d30e57
GitHub Action <github-actions[bot]@github.com>
2025-03-24 15:59:07 +0000

    Sync files 7d001a1524363184b5861b62d01b77be9da5dafb

--- bpi-r4/Banana-Pi-BPI-R4-test-2.sh
+++ bpi-r4/Banana-Pi-BPI-R4-test-2.sh
@@ -32,18 +32,18 @@
 
 
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 
 
 # rm -rf feeds/packages/devel/gn
 # rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
 
-rm -rf feeds/packages/net/adguardhome
+# rm -rf feeds/packages/net/adguardhome
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 
 
@@ -70,6 +70,22 @@ git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git pa
 # git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/packages package/imm23packages
 # mv package/imm23packages/net/adbyby package/adbyby
 # rm -rf package/imm23packages
+
+
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/devel/gn
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,geoview,hysteria,ipt2socks,microsocks,naiveproxy,pdnsd-alt,shadowsocks-rust,shadowsocksr-libev,simple-obfs,sing-box,ssocks,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns}
+rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+
+
 
 # 添加自定义软件包
 
