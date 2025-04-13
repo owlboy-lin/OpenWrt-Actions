b30002a7797632e5a152b41f84a899c34d8d0da6
owlboy-lin <298076473@qq.com>
2025-04-13 13:22:40 +0800

    Update r66s-test-2.sh

--- r66s/6.1/r66s-test-2.sh
+++ r66s/6.1/r66s-test-2.sh
@@ -251,6 +251,7 @@ echo "
 # CONFIG_PACKAGE_luci-app-openvpn=n
 # CONFIG_PACKAGE_luci-app-ramfree=y
 # CONFIG_PACKAGE_luci-app-samba4=n
+CONFIG_PACKAGE_luci-app-smartdns=n
 # CONFIG_PACKAGE_luci-app-quickstart=n
 # CONFIG_PACKAGE_luci-app-upnp=n
 # CONFIG_PACKAGE_luci-app-uugamebooster=n
