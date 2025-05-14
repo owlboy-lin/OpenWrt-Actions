32bc607dace5bd87b10ed7e24d46a8a4bf655385
owlboy-lin <298076473@qq.com>
2025-05-15 00:09:10 +0800

    Update diy-part1-test.sh

--- bpi-r4/immortalwrt/diy-part1-test.sh
+++ bpi-r4/immortalwrt/diy-part1-test.sh
@@ -41,13 +41,13 @@ merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" o
 # merge_package https://github.com/kenzok8/jell jell/luci-lib-js
 popd
 
-rm  -rf package/community/dhdaxcw-app/adguardhome
-rm  -rf package/community/dhdaxcw-app/luci-app-adguardhome
+# rm  -rf package/community/dhdaxcw-app/adguardhome
+# rm  -rf package/community/dhdaxcw-app/luci-app-adguardhome
 
-pushd package/community
-merge_package https://github.com/kenzok8/small-package small-package/adguardhome
-merge_package https://github.com/kenzok8/small-package small-package/luci-app-adguardhome
-popd
+# pushd package/community
+# merge_package https://github.com/kenzok8/small-package small-package/adguardhome
+# merge_package https://github.com/kenzok8/small-package small-package/luci-app-adguardhome
+# popd
 
 
 # Lucky
