30dcc0e8aee6085b73c6e159e238008d265d02d1
owlboy-lin <298076473@qq.com>
2025-09-14 15:31:24 +0800

    Update packages.sh

--- r66s/6.1/packages.sh
+++ r66s/6.1/packages.sh
@@ -8,7 +8,7 @@ rm -rf ./feeds/luci/applications/luci-app-mosdns
 
 #克隆依赖插件
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 
 #克隆的源码放在small文件夹
