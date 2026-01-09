4b47642e624659537717c2c5f0907a07646d60b9
owlboy-lin <298076473@qq.com>
2026-01-09 11:46:26 +0800

    update

---
+++ smpackage/Jbt-packages.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#删除feeds中的插件
+# rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/lang/golang
+rm -rf ./feeds/packages/net/{geoview,shadowsocks-libev,chinadns-ng,mosdns}
+rm -rf ./feeds/luci/applications/luci-app-mosdns
+
+#克隆依赖插件
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+
+#克隆的源码放在small文件夹
+mkdir package/small
+pushd package/small
+
+#luci-app-nft-timecontrol
+git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
+#adguardhome
+git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+#lucky
+git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
+# #smartdns
+# git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
+# git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
+# #ssrp
+git clone -b master --depth 1 https://github.com/fw876/helloworld.git
+# #passwall
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+#passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+# #mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+# #openclash
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+# OpenWrt-nikki
+git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
+# iStore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+# mv package/kz8-small/adguardhome package/adguardhome
+# mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
+mv package/kz8-small/luci-app-partexp package/luci-app-partexp
+mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+mv package/kz8-small/wrtbwmon package/wrtbwmon
+mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
+mv package/kz8-small/netspeedtest package/netspeedtest
+mv package/kz8-small/homebox package/homebox
+mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
+mv package/kz8-small/luci-app-quickstart package/luci-app-quickstart
+mv package/kz8-small/quickstart package/quickstart
+# mv package/kz8-small/luci-app-nikki package/luci-app-nikki
+# mv package/kz8-small/nikki package/nikki
+rm -rf package/kz8-small
+
+popd
+
+echo "packages executed successfully!"
