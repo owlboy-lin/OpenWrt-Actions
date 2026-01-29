eb5d4c8ad558814e7c07ebc2a9403865f07627e2
owlboy-lin <298076473@qq.com>
2026-01-29 18:07:14 +0800

    Update Jbt-Lean-packages.sh

--- smpackage/Jbt-Lean-packages.sh
+++ smpackage/Jbt-Lean-packages.sh
@@ -51,9 +51,11 @@ mv package/kz8-small/wrtbwmon package/wrtbwmon
 mv package/kz8-small/luci-app-netspeedtest package/luci-app-netspeedtest
 mv package/kz8-small/netspeedtest package/netspeedtest
 mv package/kz8-small/homebox package/homebox
+mv package/kz8-small/speedtest-cli package/speedtest-cli
 mv package/kz8-small/luci-app-poweroff package/luci-app-poweroff
 mv package/kz8-small/luci-app-quickstart package/luci-app-quickstart
 mv package/kz8-small/quickstart package/quickstart
+mv package/kz8-small/luci-app-store package/luci-app-store
 # mv package/kz8-small/luci-app-nikki package/luci-app-nikki
 # mv package/kz8-small/nikki package/nikki
 rm -rf package/kz8-small
