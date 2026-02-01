81ad965f4ead88695b38a9b7a530e23deb491b6d
owlboy-lin <298076473@qq.com>
2026-02-01 11:47:15 +0800

    Update environment.sh

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,12 +1,9 @@
 sudo apt update -qq
 sudo apt full-upgrade -qq -y
-sudo apt install -qq -y ack antlr3 asciidoc autoconf automake autopoint binutils bison \
-		build-essential bzip2 ccache cmake cpio curl device-tree-compiler ecj fakeroot \
-		fastjar flex gawk gettext genisoimage gnutls-dev gperf haveged help2man intltool \
-		irqbalance jq lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev \
-		libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libreadline-dev libssl-dev \
-		libtool libyaml-dev libz-dev lrzsz msmtp nano ninja-build p7zip p7zip-full patch \
-		pkgconf libpython3-dev python3 python3-pip python3-cryptography python3-docutils \
-		python3-ply python3-pyelftools python3-requests qemu-utils quilt re2c rsync scons \
-		sharutils squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto \
-		zlib1g-dev zstd xxd rename
+sudo apt install -qq -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
+libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev \
+libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python3 \
+python3-pyelftools python3-setuptools qemu-utils rsync scons \
+squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev rename
