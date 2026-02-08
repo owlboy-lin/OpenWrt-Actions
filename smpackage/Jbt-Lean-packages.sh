091ebde81a4497208f08d24cf657e983eff0581d
owlboy-lin <298076473@qq.com>
2026-02-08 11:26:57 +0800

    update

--- smpackage/Jbt-Lean-packages.sh
+++ smpackage/Jbt-Lean-packages.sh
@@ -7,7 +7,7 @@ cp -r /workdir/patchs/rust feeds/packages/lang
 
 #删除feeds中的插件
 rm -rf feeds/packages/lang/golang
-rm -rf ./feeds/luci/applications/{luci-app-mosdns,luci-app-passwall}
+rm -rf feeds/luci/applications/{luci-app-mosdns,luci-app-passwall}
 
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
