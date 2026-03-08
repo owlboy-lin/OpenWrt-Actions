c9c9d1df1b7c7b2e6a871a4a909527851fe8a3dc
owlboy-lin <298076473@qq.com>
2026-03-08 14:14:24 +0800

    Update BLLW-init-settings.sh

--- scripts/BLLW-init-settings.sh
+++ scripts/BLLW-init-settings.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-#更改默认地址为192.168.89.10
+#更改默认地址为192.168.8.1
 sed -i 's/192.168.1.1/192.168.89.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.89.10/g' package/base-files/luci/bin/config_generate
 
 # 给config下的文件增加权限
 chmod 644 files/etc/config/*
 
+#更改design主题为白色
+# sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
 
 echo "开始处理 homeproxy 规则和 DNS 配置..."
 echo "========================================"
