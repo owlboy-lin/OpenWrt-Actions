c02c208b7397518248d5dcc446d4e210e7c99643
owlboy-lin <298076473@qq.com>
2026-04-22 10:35:28 +0800

    Update My-LEDE-packages-mosdns.sh

--- scripts/My-LEDE-packages-mosdns.sh
+++ scripts/My-LEDE-packages-mosdns.sh
@@ -88,8 +88,8 @@ git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
 # git clone -b main --depth 1 https://github.com/esirplayground/luci-app-poweroff.git
 
 # kz8-small
-# git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
-# mv package/kz8-small/adguardhome package/adguardhome
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/adguardhome package/adguardhome
 # mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
 # mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
 # mv package/kz8-small/luci-app-partexp package/luci-app-partexp
@@ -108,7 +108,7 @@ git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
 # mv package/kz8-small/taskd package/taskd
 # mv package/kz8-small/luci-app-nikki package/luci-app-nikki
 # mv package/kz8-small/nikki package/nikki
-# rm -rf package/kz8-small
+rm -rf package/kz8-small
 
 popd
 
