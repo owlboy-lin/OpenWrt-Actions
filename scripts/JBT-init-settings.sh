483f47cdc3759d566f8f2d374796190d334507bb
owlboy-lin <298076473@qq.com>
2026-07-19 17:16:43 +0800

    Update JBT-init-settings.sh

--- scripts/JBT-init-settings.sh
+++ scripts/JBT-init-settings.sh
@@ -2,10 +2,13 @@
 
 #更改默认地址为192.168.24.10
 sed -i 's/192.168.1.1/192.168.24.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.24.10/g' package/base-files/luci/bin/config_generate
 
 # 给config下的文件增加权限
 chmod 644 files/etc/config/*
 
+#更改design主题为白色
+# sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
 
 echo "开始处理 homeproxy 规则和 DNS 配置..."
 echo "========================================"
