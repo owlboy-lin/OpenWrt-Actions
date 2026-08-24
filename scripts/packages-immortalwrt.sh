1b1a8fea34a66eeef7841f67b1d4fa519b13c99b
owlboy-lin <298076473@qq.com>
2026-08-24 19:28:10 +0800

    update

--- scripts/packages-immortalwrt.sh
+++ scripts/packages-immortalwrt.sh
@@ -28,39 +28,39 @@ git clone -b main --depth 1  https://github.com/kenzok78/luci-app-adguardhome
 # git clone -b master --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git
 
 # lucky
-git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
+# git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
 
 # smartdns
 # git clone -b master --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
 
 # ssrp
-git clone -b master --depth 1 https://github.com/fw876/helloworld.git
+# git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 
 # VIKINGYFY/packages
 git clone -b main --depth 1 https://github.com/VIKINGYFY/packages.git
 
 # passwall
-git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall.git
+# git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall.git
 
 # passwall2
-git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+# git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2.git
 
 # mosdns
 git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 
 # luci-app-netspeedtest
-# git clone -b master --depth 1 https://github.com/muink/luci-app-netspeedtest.git
+#git clone -b master --depth 1 https://github.com/muink/luci-app-netspeedtest.git
 git clone -b main --depth 1  https://github.com/sirpdboy/netspeedtest.git
 
 # openclash
-git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 
 # OpenWrt-nikki
-git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
+# git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
 
 # OpenWrt-momo
-git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo.git
+# git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo.git
 
 # daed
 git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
