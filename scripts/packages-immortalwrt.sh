d68eb947f02ee00c72ae913023590e79b2187b54
owlboy-lin <298076473@qq.com>
2026-08-25 11:18:37 +0800

    Update packages-immortalwrt.sh

--- scripts/packages-immortalwrt.sh
+++ scripts/packages-immortalwrt.sh
@@ -67,7 +67,7 @@ git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
 
 # kz8-small
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
-# mv package/kz8-small/adguardhome package/adguardhome
+mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
 # mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
 # mv package/kz8-small/luci-app-partexp package/luci-app-partexp
