bbdfe645ecc81d5aa94e5770da1e8f2dde86cc70
owlboy-lin <298076473@qq.com>
2026-09-07 12:38:24 +0800

    Update packages-LEDE-mosdns.sh

--- scripts/packages-LEDE-mosdns.sh
+++ scripts/packages-LEDE-mosdns.sh
@@ -88,8 +88,8 @@ git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall
 
 
 # luci-app-netspeedtest
-# git clone -b master --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest.git
-git clone -b main --depth 1  https://github.com/sirpdboy/netspeedtest.git
+git clone -b master --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest.git
+# git clone -b main --depth 1  https://github.com/sirpdboy/netspeedtest.git
 
 # openclash
 # git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
